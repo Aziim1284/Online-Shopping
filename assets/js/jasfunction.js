fec584e1ba6e363294af0074cbde961b3e0fb1f6
Aziim1284 <aziimbhatti613@gmail.com>
2022-03-15 12:03:26 +0530

    Updated

---
+++ assets/js/jasfunction.js
@@ -0,0 +1,15 @@
+  console.log("Functions ion js");
+  function one(name){ }
+      console.log(name + "this is js function");
+
+ 
+  let name = "aziim";
+  let name1 = "rahul";
+  let name2 = "pakora";
+  let name3 = "dana";
+ 
+  one(name) ;
+  one(name1);
+  one(name2);
+  one(name3);
+ 
